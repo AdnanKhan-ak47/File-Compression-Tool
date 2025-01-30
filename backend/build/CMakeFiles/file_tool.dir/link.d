9b6d59625012eaaee61ed8796d3de487cde69a44
Adnan Khan <adnankhan07666@gmail.com>
2025-01-30 08:33:12 +0530

    Fixed Bug in decompression file

---
+++ backend/build/CMakeFiles/file_tool.dir/link.d
@@ -0,0 +1,160 @@
+file_tool: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o \
+  CMakeFiles/file_tool.dir/src/main.cpp.o \
+  CMakeFiles/file_tool.dir/src/compress_file.cpp.o \
+  CMakeFiles/file_tool.dir/src/decompress_file.cpp.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libarchive.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o \
+  /usr/lib32/libcrypto.so.3 \
+  /usr/lib/libcrypto.so.3 \
+  /usr/lib32/libacl.so.1 \
+  /usr/lib/libacl.so.1 \
+  /usr/lib32/liblzma.so.5 \
+  /usr/lib/liblzma.so.5 \
+  /usr/lib32/libzstd.so.1 \
+  /usr/lib/libzstd.so.1 \
+  /usr/lib/liblz4.so.1 \
+  /usr/lib32/libbz2.so.1.0 \
+  /usr/lib/libbz2.so.1.0 \
+  /usr/lib32/libz.so.1 \
+  /usr/lib/libz.so.1 \
+  /usr/lib32/libxml2.so.2 \
+  /usr/lib/libxml2.so.2 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib32/libicuuc.so.75 \
+  /usr/lib/libicuuc.so.75 \
+  /usr/lib32/libicudata.so.75 \
+  /usr/lib/libicudata.so.75
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o:
+
+CMakeFiles/file_tool.dir/src/main.cpp.o:
+
+CMakeFiles/file_tool.dir/src/compress_file.cpp.o:
+
+CMakeFiles/file_tool.dir/src/decompress_file.cpp.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libarchive.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o:
+
+/usr/lib32/libcrypto.so.3:
+
+/usr/lib/libcrypto.so.3:
+
+/usr/lib32/libacl.so.1:
+
+/usr/lib/libacl.so.1:
+
+/usr/lib32/liblzma.so.5:
+
+/usr/lib/liblzma.so.5:
+
+/usr/lib32/libzstd.so.1:
+
+/usr/lib/libzstd.so.1:
+
+/usr/lib/liblz4.so.1:
+
+/usr/lib32/libbz2.so.1.0:
+
+/usr/lib/libbz2.so.1.0:
+
+/usr/lib32/libz.so.1:
+
+/usr/lib/libz.so.1:
+
+/usr/lib32/libxml2.so.2:
+
+/usr/lib/libxml2.so.2:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib32/libicuuc.so.75:
+
+/usr/lib/libicuuc.so.75:
+
+/usr/lib32/libicudata.so.75:
+
+/usr/lib/libicudata.so.75:
--- backend/src/decompress_file.cpp
+++ backend/src/decompress_file.cpp
@@ -1,38 +1,49 @@
-
-#include "decompress_file.h"
 #include <archive.h>
 #include <archive_entry.h>
+#include <fcntl.h>
 #include <iostream>
-#include <string>
+#include <unistd.h>
 
-void decompress_file(const std::string &archive_name,
-                     const std::string &output_dir) {
+void decompress_file(const std::string &archivePath,
+                     const std::string &outputDir) {
   struct archive *archive = archive_read_new();
   struct archive *disk = archive_write_disk_new();
   struct archive_entry *entry;
+  int result;
 
+  // Use the correct function for enabling all filters
+  archive_read_support_filter_all(archive);
   archive_read_support_format_all(archive);
-  archive_read_support_compression_all(archive);
 
-  if (archive_read_open_filename(archive, archive_name.c_str(), 10240) !=
-      ARCHIVE_OK) {
+  // Open the archive file
+  result = archive_read_open_filename(archive, archivePath.c_str(), 10240);
+  if (result != ARCHIVE_OK) {
     std::cerr << "Failed to open archive: " << archive_error_string(archive)
-              << "\n";
+              << std::endl;
     return;
   }
 
   while (archive_read_next_header(archive, &entry) == ARCHIVE_OK) {
-    const char *current_file = archive_entry_pathname(entry);
-    std::string output_path = output_dir + "/" + current_file;
-    archive_entry_set_pathname(entry, output_path.c_str());
+    std::string outputPath = outputDir + "/" + archive_entry_pathname(entry);
+    archive_entry_set_pathname(entry, outputPath.c_str());
 
-    if (archive_write_header(disk, entry) != ARCHIVE_OK) {
+    result = archive_write_header(disk, entry);
+    if (result != ARCHIVE_OK) {
       std::cerr << "Failed to write header: " << archive_error_string(disk)
-                << "\n";
-      break;
-    }
+                << std::endl;
+    } else {
+      // Corrected: Read data and write it manually
+      const void *buff;
+      size_t size;
+      la_int64_t offset;
+      int fd = open(outputPath.c_str(), O_WRONLY | O_CREAT, 0644);
 
-    archive_copy_data(archive, disk);
+      while (archive_read_data_block(archive, &buff, &size, &offset) ==
+             ARCHIVE_OK) {
+        write(fd, buff, size);
+      }
+      close(fd);
+    }
     archive_write_finish_entry(disk);
   }
 
@@ -40,7 +51,4 @@ void decompress_file(const std::string &archive_name,
   archive_write_close(disk);
   archive_read_free(archive);
   archive_write_free(disk);
-
-  std::cout << "Decompressed: " << archive_name << " into " << output_dir
-            << "\n";
 }
